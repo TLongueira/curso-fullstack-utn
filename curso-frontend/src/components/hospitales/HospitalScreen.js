9567436b7c0d81acbd1276f9f78c354a7729180f
TLongueira <tiagolongueira@gmail.com>
2021-12-06 15:43:36 -0300

    react-router-dom

--- curso-frontend/src/components/hospitales/HospitalScreen.js
+++ curso-frontend/src/components/hospitales/HospitalScreen.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import { MedicosList } from '../medicos/MedicosList';
-import { HospitalesList } from './HospitalesList';
 
 export const HospitalScreen = ({ history }) => {
     const { hospitalId } = useParams();
--- curso-frontend/src/components/medicos/MedicoScreen.js
+++ curso-frontend/src/components/medicos/MedicoScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import { HospitalesList } from '../hospitales/HospitalesList';
 
 export const MedicoScreen = ({ history }) => {
